0b33b5f0c8877f437f6b725a3d7f2e2ddb631f75
tatyana86 <krivonogova_1999@mail.ru>
2024-05-16 22:29:43 +0400

    update gitignore

--- src/main/java/ru/krivonogova/autopark/bot/BotConfig.java
+++ src/main/java/ru/krivonogova/autopark/bot/BotConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Data;
 
 @Data
@@ -12,13 +11,10 @@ import lombok.Data;
 @PropertySource("classpath:application.properties")
 public class BotConfig {
 	
-	@Value("${bot.name}")
-    private String botName;
-    private String token;
+    @Value("${bot.name}")
+    String botName;
 
-    public BotConfig() {
-        Dotenv dotenv = Dotenv.load();
-        this.token = dotenv.get("TELEGRAM_BOT_TOKEN"); // Получаем значение из .env
-    }
+    @Value("${bot.token}")
+    String token;
 
 }
